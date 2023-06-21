8c74c84db0f4c0f4047f43022968f04b4170aa8c
Greg Garcia <greg@garcia.casa>
2023-06-21 00:00:26 -0400

    Add: styling, background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,32 +27,32 @@ export default function Home() {
 
     return (
         <>
-            <main className={`bg-gradient min-h-screen flex flex-col items-center py-12 px-6 md:px-12 lg:px-24 overflow-hidden flex-grow`}>
+            <main className={`half-and-half-background min-h-screen flex flex-col items-center py-12 px-6 md:px-12 lg:px-24 overflow-hidden flex-grow`}>
                 <ToastContainer />
                 <h1 className="text-5xl font-extrabold tracking-tight mb-6 font-mono">Func-it</h1>
-<p className="text-white text-center text-lg mb-4">
-    Generate a Chat Completions API <code className=" text-gray-300 font-semibold ">functions</code> parameter from your existing API.
-</p>
-<p className="text-white text-center text-sm mb-12">
-    Quickly get started with <a href="https://platform.openai.com/docs/guides/gpt/function-calling" target="_blank" rel="noopener noreferrer" className=" hover:text-zinc-300 text-zinc-200 font-semibold ">
-        function calling
-        <svg xmlns="http://www.w3.org/2000/svg" className="inline-block w-4 h-4 ml-1 mb-1" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M15 5v6h-1V6H5v8h6v1H4V4h11z" />
-        </svg>
-    </a> enabled GPT models by pasting your API specification and converting it to a ready-to-use <code className="text-gray-300 font-semibold">functions</code> parameter in the language of your choice.
-</p>
-                <div className="w-full flex flex-col md:flex-row md:justify-between m-6">
+                <p className="text-white text-center text-lg mb-4">
+                    Generate a Chat Completions API <code className=" text-gray-300 font-semibold ">functions</code> parameter from your existing API.
+                </p>
+                <p className="text-white text-center text-sm mb-2 lg:w-8/12  md:w-3/5 sm:w-1/2">
+                    Quickly get started with <a href="https://platform.openai.com/docs/guides/gpt/function-calling" target="_blank" rel="noopener noreferrer" className=" hover:text-zinc-300 text-zinc-200 font-semibold ">
+                        function calling
+                        <svg xmlns="http://www.w3.org/2000/svg" className="inline-block w-4 h-4 ml-1 mb-1" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M15 5v6h-1V6H5v8h6v1H4V4h11z" />
+                        </svg>
+                    </a> enabled GPT models by pasting your API specification and converting it to a ready-to-use <code className="text-gray-300 font-semibold">functions</code> parameter in the language of your choice.
+                </p>
+                <div className="w-full flex flex-col md:flex-row md:justify-between m-2">
                     <div className="flex flex-col items-center md:items-start mb-4 md:mb-0">
-                        <p className="text-sm text-white mb-2">Input language</p>
+                        <p className="text-sm text-white mb-2 font-semibold">Input language</p>
                         <InputLanguageSelector onLanguageChange={handleInputLanguageChange} />
                     </div>
                     <div className="flex flex-col items-center md:items-start">
-                        <p className="text-sm text-white mb-2">Output language</p>
+                        <p className="text-sm text-white mb-2 font-semibold">Output language</p>
                         <OutputLanguageSelector onLanguageChange={handleOutputLanguageChange} />
                     </div>
                 </div>
                 <Completion inputLanguage={inputLanguage} outputLanguage={outputLanguage} />
-                <span className="text-sm">View on <a href="https://openai.com/" target="_blank" className="underline">GitHub</a></span>
+                <span className="text-sm">View on <a href="https://github.com/iamgreggarcia/funcit/tree/main" target="_blank" className="underline">GitHub</a></span>
 
             </main>
 
--- src/components/completion.tsx
+++ src/components/completion.tsx
@@ -5,7 +5,6 @@ import { useCompletion } from 'ai/react';
 import { CodeBlock } from "@/components/codeblock"
 import { OPENAPI_JSON } from '../utils/const'
 
-
 enum InputLanguage {
   Natural = 'Natural Language',
   YAML = 'yaml',
@@ -59,29 +58,40 @@ export default function Completion({ inputLanguage, outputLanguage }: Completion
     <div className="mx-auto w-full flex flex-col h-screen pb-8">
       <form className="flex flex-grow overflow-hidden" onSubmit={handleSubmit}>
         <div className="flex flex-col md:flex-row justify-around w-full h-5/6">
-          <div className="w-full md:w-3/5 h-1/2 md:h-full overflow-auto bg-[#21202e] mb-4 md:mb-0">
+          <div className=" w-full md:w-3/5 h-1/2 md:h-full overflow-auto bg-[#21202e] shadow-xl mb-4 md:mb-0">
             <CodeBlock code={firstCodeBlockValue} onChange={handleFirstCodeBlockChange} />
           </div>
           <div className="flex flex-col justify-center mx-4">
-            {isLoading && (
-              <button type="button" onClick={stop} className="relative bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4 transform transition duration-500 ease-in-out hover:scale-105">
-                <svg className="absolute top-0 left-0 h-full w-full" viewBox="0 0 150 50">
-                  <path className="stroke-current stroke-2" d="M5 5 H 145 V 45 H 5 Z" fill="transparent" strokeDasharray="300" strokeDashoffset="300">
-                    <animate attributeName="stroke-dashoffset" from="300" to="0" dur="2s" repeatCount="indefinite" />
-                  </path>
-                </svg>
-                STOP GENERATING
-              </button>
-            )}
-            <button
-              disabled={isLoading}
-              type="submit"
-              className={`relative bg-[#f0f3f4] hover:bg-[#d5eaf0] text-[#312f2f] hover:text-[#302f2f] font-bold py-2 px-12 rounded transform transition duration-500 ease-in-out ${isLoading ? 'opacity-50 cursor-not-allowed' : 'hover:scale-105'}`}
-            >
-              CONVERT
-            </button>
+
+            {!isLoading && (
+              <button
+                disabled={isLoading}
+                type="submit"
+                className='relative bg-[#f0f3f4] hover:bg-[#d5eaf0] text-[#312f2f] hover:text-[#302f2f] font-bold py-2 px-8 rounded transform transition duration-500 ease-in-out hover:scale-105'
+              >
+                <h3 className='flex flex-row justify-between'>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-4">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
+                  </svg>
+                  GENERATE</h3>
+              </button>) || (
+                <button
+                  type="button"
+                  onClick={stop}
+                  className="relative bg-[#f0f3f4] hover:bg-[#d5eaf0] text-[#312f2f] hover:text-[#302f2f] font-bold py-2 px-4 rounded mb-4 transform duration-500 ease-in-out hover:scale-105"
+                >
+                                  <h3 className='flex flex-row justify-between'>
+
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-4">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z" />
+                  </svg>
+                  STOP
+                  </h3>
+                </button>
+              )}
+
           </div>
-          <div className="w-full md:w-3/5 h-1/2 md:h-full overflow-auto bg-[#21202e] mt-4 md:mt-0">
+          <div className="w-full md:w-3/5 h-1/2 md:h-full overflow-auto bg-[#21202e] shadow-xl mt-4 md:mt-0">
             <CodeBlock code={secondCodeBlockValue} editable={false} showCopy={true} />
           </div>
         </div>
